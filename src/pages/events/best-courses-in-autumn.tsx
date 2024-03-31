ae02fc6fe12632fc94eca3368fa4bb8a7215d110
Umut Öztürk <122098251+umutZT@users.noreply.github.com>
2024-04-01 00:13:35 +0300

    Update best-courses-in-autumn.tsx

--- src/pages/events/best-courses-in-autumn.tsx
+++ src/pages/events/best-courses-in-autumn.tsx
@@ -86,6 +86,7 @@ const BestCourse = () => {
               
                     </div>
                   </div>
+                </div>
                 
               </TabPane>
               </TabPane>
